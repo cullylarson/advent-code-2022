09531670def4ca095d66087c9a55b1cc6be052b3
Cully Larson <cully.larson@gmail.com>
2022-12-02 22:17:22 -0800

    Generalize finding common values in day 3, part 2.

--- day-03/part-2.js
+++ day-03/part-2.js
@@ -1,13 +1,13 @@
-import {compose, report, map, head, curry} from '@cullylarson/f'
+import {compose, report, map, head, curry, tail} from '@cullylarson/f'
 import {then} from '@cullylarson/p'
 import {readInput, getPriority} from './lib.js'
 import {rel, sum} from '../lib.js'
 
-const findCommon = ([a, b, c]) => {
-  const aSet = new Set(a)
-  const bSet = new Set(b)
-  const cSet = new Set(c)
-  return [...aSet].filter(x => bSet.has(x) && cSet.has(x))
+const findCommon = (arrs) => {
+  const sets = arrs.map(x => new Set(x))
+  const headSet = head(sets)
+  const tailSets = tail(sets)
+  return [...headSet].filter(x => tailSets.every(y => y.has(x)))
 }
 
 const group = curry((n, xs) => xs.reduce((acc, x, i) => {
